f9aad99eafca6664386af273921ae22640ad9a0d
José Augusto <joseaugusto0408@gmail.com>
2021-09-07 11:49:19 -0300

    exercicio 4 - 1 semana

---
+++ exercicio-4-1.js
@@ -0,0 +1,6 @@
+// crie uma função que receba um parâmetro numérico e converta ele de celsius para fahrenheit.
+// Dica: 1 grau celsius é igual a 33.8 graus fahrenheit
+
+const celsius = (num) => (9*num+160)/5;
+
+console.log(celsius(1));
---
+++ exercicio-4-2.js
@@ -0,0 +1,8 @@
+// Crie uma outra função que receba um texto como parâmetro e retorne o número de palavras que tem nesse texto.
+
+
+function contadorPalavras(text) {
+    return text.length;
+}
+
+console.log(contadorPalavras('Mentoria'));
