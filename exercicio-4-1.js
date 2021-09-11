adf88318fdb8fb604001cb351f9b0f7eb9982047
José Augusto <joseaugusto0408@gmail.com>
2021-09-11 07:56:21 -0300

    fixed: changing logic

--- exercicio-4-1.js
+++ exercicio-4-1.js
@@ -1,6 +1,6 @@
 // crie uma função que receba um parâmetro numérico e converta ele de celsius para fahrenheit.
 // Dica: 1 grau celsius é igual a 33.8 graus fahrenheit
 
-const celsius = (num) => (9*num+160)/5;
+const fahrenheit = (celsius) => (9*celsius/5)+32;
 
-console.log(celsius(1));
+console.log(fahrenheit(33.8));
