e6a8ef500c073a0cf7244826c53ae540416576dc
José Augusto <joseaugusto0408@gmail.com>
2021-09-11 10:21:53 -0300

    exercicio 9

---
+++ exercicio-9.js
@@ -0,0 +1,13 @@
+// Crie uma função com uma variável chamada num e incremente o valor dentro de um setInterval de 1200ms. Ela deve mostrar no console cada atualização até que o programa seja encerrado.
+// Dica: quando você está executando um programa no console e ele não termina, você pode forçar o encerramento dele apertando ctrl+c
+
+function incrementNum() {
+  let num = 0;
+  setInterval(() => {
+    while (true) {
+      num++;
+      console.log(num);
+    }
+  }, 1200);
+}
+incrementNum()
